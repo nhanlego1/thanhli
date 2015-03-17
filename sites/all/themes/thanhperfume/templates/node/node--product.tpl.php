e52ffe4e79031bc496aeba625335ffcef1b3687a
nhan <nhanlego1@gmail.com>
2015-03-17 09:52:53 +0700

    update node template

---
+++ sites/all/themes/thanhperfume/templates/node/node--product.tpl.php
@@ -0,0 +1,37 @@
+<article<?php print $attributes; ?>>
+    <?php print $user_picture; ?>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page && $title): ?>
+        <header>
+            <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+        </header>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+
+    <div<?php print $content_attributes; ?>>
+        <div class="content-product-top grid-9">
+            <div class="image-cloud grid-4">
+                <?php print render($content['field_product_image']); ?>
+            </div>
+            <div class="small-info grid-4">
+                <header>
+                    <h2<?php print $title_attributes; ?>><?php print $node->title ?></h2>
+                </header>
+
+
+            </div>
+        </div>
+        <h2 class="detail-info">Thông tin sản phẩm </h2>
+        <div class="content-product-bottom grid-8">
+
+            <?php
+            // We hide the comments and links now so that we can render them later.
+            hide($content['comments']);
+            hide($content['links']);
+            print render($content);
+            ?></div>
+
+    </div>
+
+
+</article>

d0668a60af9cb72b6830774ea4a9b7d796dcf3fe
nhan <nhanlego1@gmail.com>
2015-03-29 15:47:29 +0700

    update code new

---
+++ sites/all/modules/custom/merc_global/js/merc_global.js
@@ -0,0 +1,11 @@
+(function($) {
+
+    Drupal.behaviors.mercvn = {
+        attach: function(context, settings) {
+           $("#block-views-exp-products-page-3 input#edit-title").attr('placeholder','Tìm sản phẩm');
+        }
+    }
+
+
+
+})(jQuery);
